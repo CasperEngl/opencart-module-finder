e839c6066dc1b3b8b372ddcad8fcbbc1bc3846a6
Casper <me@casperengelmann.com>
2019-11-08 10:45:00 +0100

    added doc

--- src/wait.ts
+++ src/wait.ts
@@ -1,3 +1,7 @@
+/**
+ * Used to sleep code processing
+ * @param waitTime In ms
+ */
 export const wait = (waitTime: number): Promise<void> => new Promise(
   (resolve): NodeJS.Timeout => setTimeout(
     () => resolve(), waitTime,
